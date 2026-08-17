43c7d9d66431d3f12be0947bf365704ac1d5a123
Alan Miaou <mjoalen@gmail.com>
2026-08-17 12:25:59 +0800

    feat(db): PromotionRules 新增 effect_reward_type 欄位的 PG migration
    grant_reward 效果的「獎項類型」下拉（對齊 UserRewards.rewardType 封閉
    enum）需要這個欄位才會生效。13 行、單一欄位＋對應 enum type，基準取自
    目前線上 commit 7ffc546，未摻入其他未部署工作。

--- src/migrations-pg/index.ts
+++ src/migrations-pg/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260816_023522_pg_baseline from './20260816_023522_pg_baseline';
 import * as migration_20260817_035151_p0b_rule_conditions from './20260817_035151_p0b_rule_conditions';
+import * as migration_20260817_042534_p0b_reward_type from './20260817_042534_p0b_reward_type';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260817_035151_p0b_rule_conditions.up,
     down: migration_20260817_035151_p0b_rule_conditions.down,
-    name: '20260817_035151_p0b_rule_conditions'
+    name: '20260817_035151_p0b_rule_conditions',
+  },
+  {
+    up: migration_20260817_042534_p0b_reward_type.up,
+    down: migration_20260817_042534_p0b_reward_type.down,
+    name: '20260817_042534_p0b_reward_type'
   },
 ];
